30de9af39a2bb7c16fe2c72390b3859149ce1724
khszone02 <khszone02@naver.com>
2016-07-26 13:05:39 +0900

    git add folders

--- test.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const app = express();
-
-app.post('/reply', (req, res)=>{
-    const postId = req.query.postId;
-    console.log(postId);
-});
-
-app.listen(3000);
