82689d80dfe2384ecc51f357d0a83c4e1722d37d
khszone02 <khszone02@naver.com>
2016-08-02 17:13:49 +0900

    2016.08.02 In addition, complement mylike of result by for-loop

--- model/posts.js
+++ model/posts.js
@@ -17,8 +17,7 @@ class Post{}
 //         .then((results)=>{
 //             const friendList = results.friendList;
 //             console.log('FRIEDNLIST', friendList)
-            
-//             post.find({_id:{$lt: endPost}})
+//             post.find({_id:{$lt: mongoose.Types.ObjectId(endPost)}})
 //             .where('userId').in(friendList)
 //             .limit(count)
 //             .then((results)=>{
@@ -30,28 +29,27 @@ class Post{}
 //             callback(err, null);
 //     });
 // }
-Post.getPosts = function(endPost, userId, count, callback){
-    if (!endPost){
-        endPost = "ffce5eef0000000000000000"; // ObjectId of 2105.12.31 23:59:59
-    }
-    user.findOne({_id : userId}, 'friendList')
-        .then((results)=>{
-            const friendList = results.friendList;
-            console.log('FRIEDNLIST', friendList);
-            post.aggregate()
-            .unwind('$postLikeUsers')
-            .lookup({from : 'users', localField : 'postLikeUsers', foreignField : '_id', as : 'myLike'})
-            .match({_id:{$lt: mongoose.Types.ObjectId(endPost)}, userId :{$in: friendList}})
-            .limit(count)   
-            .then((results)=>{
-                callback(null, results);
-            }, (err)=>{
-                callback(err, null);
-            });
-        }, (err)=>{
-            callback(err, null);
-    });
-}
+///////////////////////////////////////////////////////// 그냥 다 긁어와서 for문 돌려서 mylike 넣기
+// Post.getPosts = function(endPost, userId, count, callback){
+//     if (!endPost){
+//         endPost = "ffce5eef0000000000000000"; // ObjectId of 2105.12.31 23:59:59
+//     }
+//     user.findOne({_id : userId}, 'friendList')
+//         .then((results)=>{
+//             const friendList = results.friendList;
+//             console.log('FRIEDNLIST', friendList);
+//             post.aggregate()
+//             .match({_id:{$lt: mongoose.Types.ObjectId(endPost)}, userId :{$in: friendList}})
+//             .limit(count)   
+//             .then((results)=>{
+//                 callback(null, results);
+//             }, (err)=>{
+//                 callback(err, null);
+//             });
+//         }, (err)=>{
+//             callback(err, null);
+//     });
+// }
 
 Post.getPosts(null, "57a04ad314779f0c027a9b0e", 5, (err, result)=>{
     if (err){
